0ca031a0f4ac76eb0212b56a41d7964e29d2f322
Devious Tanuki <leonhard.kueper@tu-dortmund.de>
2025-12-17 15:47:01 +0100

    Fix Base URL

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,10 +1,10 @@
 export default function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/11ty/css");
-    eleventyConfig.addGlobalData("baseUrl", process.env.ELEVENTY_ENV === "production" ? "/BBData/" : "/");
-    return {
-        dir: {
-            input: "src/11ty",
-            output: "_site",
-        }
-    };
+  eleventyConfig.addPassthroughCopy("src/11ty/css");
+  eleventyConfig.addGlobalData("BASE_URL", process.env.BASE_URL || "/");
+  return {
+    dir: {
+      input: "src/11ty",
+      output: "_site",
+    }
+  };
 }
