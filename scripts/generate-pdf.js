fad88cb0f76198790b953ee4cc09c65f2deacc0a
Devious Tanuki <leonhard.kueper@tu-dortmund.de>
2026-01-02 18:00:50 +0100

    WIP

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -9,35 +9,20 @@ const url = `http://localhost:3000${BASE_URL}cheat_sheets/index.html`;
 const output = path.resolve('_site/cheat_sheets/cheat_sheets.pdf');
 
 const server = http.createServer((request, response) => {
-  const { method, url } = request;
-  response.on('finish', () => {
-    console.log(`${method} ${url} -> ${response.statusCode}`);
-  });
-  return handler(request, response, {
-    public: siteDir,
-    cleanUrls: false,
-    rewrites: BASE_URL === '/'
-      ? []
-      : [{ source: `${BASE_URL}**`, destination: '/$1' }],
-  });
-});
-
-const server2 = http.createServer((request, response) => {
-  const { method } = request;
-  let { url } = request;
+  let { method, url } = request;
 
   response.on('finish', () => {
     console.log(`${method} ${url} -> ${response.statusCode}`);
   });
 
-  // if (BASE_URL !== '/') {
-  //   if (!url.startsWith(BASE_URL)) {
-  //     response.statusCode = 404;
-  //     response.end();
-  //     return;
-  //   }
-  //   request.url = url.slice(BASE_URL.length - 1);
-  // }
+  if (BASE_URL !== '/') {
+    if (!url.startsWith(BASE_URL)) {
+      response.statusCode = 404;
+      response.end();
+      return;
+    }
+    request.url = url.slice(BASE_URL.length - 1);
+  }
 
   return handler(request, response, {
     public: siteDir,
@@ -45,7 +30,7 @@ const server2 = http.createServer((request, response) => {
   });
 });
 
-server2.listen(3000, async () => {
+server.listen(3000, async () => {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
